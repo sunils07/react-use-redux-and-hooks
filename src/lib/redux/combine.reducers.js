ee080c844ab60560bb59067939d4dec10eff0960
Sunil Dandwate <sunil.dandwate@theaa.com>
2020-01-31 15:50:42 +0000

    Remove unwanted

--- src/lib/redux/combine.reducers.js
+++
@@ -1,5 +0,0 @@
-export default function combineReducer(reducers, dispatchActions) {
-    return {
-        ...reducers
-    }
-}
--- src/lib/redux/root.reducer.js
+++
@@ -1,7 +0,0 @@
-import combineReducer from "./combine.reducers";
-
-import useMathReducer from "./modules/math/math.reducer";
-
-export default combineReducer({
-    useMathReducer: useMathReducer
-});
